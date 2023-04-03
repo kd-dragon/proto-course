df6c9d6abe291e5eec44ac4f6422729cd921f032
Ray.Kim <ray.kim@themedium.io>
2023-04-03 16:58:19 +0900

    [grpc-go] feat: Create Boilerplate gRPC server&client

---
+++ grpc-go-course/README.md
@@ -0,0 +1,1 @@
+<h3> gRPC Go Course </h3>
---
+++ grpc-go-course/greet/server/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	pb "github.com/kd-dragon/proto-course/grpc-go-course/greet/proto"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+)
+
+var addr string = "0.0.0.0:50051"
+
+type Server struct {
+	pb.GreetServiceServer
+}
+
+func main() {
+	lis, err := net.Listen("tcp", addr)
+	if err != nil {
+		log.Fatalf("Failed to listen: %v\n", err)
+	}
+
+	log.Printf("Server listening on %v\n", addr)
+
+	s := grpc.NewServer()
+	if err = s.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v\n", err)
+	}
+}
